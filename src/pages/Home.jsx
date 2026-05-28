1693a047aa29140d5adc8a3b2f50270271c651ae
nw25aao <nw25aao@herts.ac.uk>
2026-05-28 23:57:34 +0530

    updated skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navigation from "../components/Navigation";
 import About from "../sections/About";
 import Contact from "../sections/Contact";
-import Hero from "../sections/hero";
+import Hero from "../sections/Hero";
 import Projects from "../sections/Projects";
 import Skills from "../sections/Skills";
 
