918255109581ca6888f68017ef8169d2caf24d88
nw25aao <nw25aao@herts.ac.uk>
2026-05-29 15:54:43 +0530

    added the updated logo

--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -10,15 +10,10 @@ export default function Resume() {
       <Layout sectionName={"CV/Resume"} id={"contact"}>
         <div className="flex flex-col gap-y-10 justify-center items-center rounded-lg">
           <PdfViewer></PdfViewer>
-          <a
-            href="/Nohim-hasitha-cv.pdf"
-            download="Nohim-hasitha-cv.pdf"
-            className="rounded-lg  px-5 py-3 text-white"
-          >
-            <DownloadCVButton className=" bg-blue-700 hover:bg-blue-400 border-0">
-              Download CV
-            </DownloadCVButton>
-          </a>
+
+          <DownloadCVButton className=" bg-blue-700 hover:bg-blue-400 border-0">
+            Download CV
+          </DownloadCVButton>
         </div>
       </Layout>
     </>
