129a309b9c3e216830f84858919b6e4ffa7930fe
nw25aao <nw25aao@herts.ac.uk>
2026-05-29 08:23:30 +0530

    added pdf

--- src/sections/Resume.jsx
+++ src/sections/Resume.jsx
@@ -1,7 +1,7 @@
 import Button from "../components/Button";
 import PdfViewer from "../components/PdfViewer";
 import Layout from "./Layout";
-import cvPdf from "../assets/Nohim-hasitha-cv.pdf";
+import cvPdf from "/Nohim-hasitha-cv.pdf";
 
 export default function Resume() {
   return (
@@ -9,7 +9,11 @@ export default function Resume() {
       <Layout sectionName={"CV/Resume"} id={"contact"}>
         <div className="flex flex-col gap-y-10 justify-center items-center rounded-lg">
           <PdfViewer file={cvPdf}></PdfViewer>
-          <a href={cvPdf} download="Nohim-CV.pdf">
+          <a
+            href="/Nohim-hasitha-cv.pdf"
+            download="Nohim-hasitha-cv.pdf"
+            className="rounded-lg bg-blue-700 px-5 py-3 text-white hover:bg-blue-500"
+          >
             <Button className="border-gray-700 bg-blue-700 hover:bg-blue-400">
               Download CV
             </Button>
